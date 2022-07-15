18caba9c1603815ef9cae35151d1d63f5e6cd6f0
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2022-07-15 09:28:25 +0100

    - Forecast not needed

--- Polymorphic.Models/WeatherForecast.cs
+++ Polymorphic.Models/WeatherForecast.cs
@@ -1,8 +1,10 @@
 namespace Polymorphic.Models;
 
-public abstract class WeatherForecast : Forecast
+public abstract class WeatherForecast
 {
+  public DateTime Date { get; set; }
+  public string? Summary { get; set; }
+  public WeatherForecast? Previous { get; set; }
   public int TemperatureC { get; set; }
-
   public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
